b7c0828e263bf6c28fea2c3839538b0229ebb11f
simon <simon.schrodi09@gmail.com>
2020-07-03 15:04:35 +0200

    Fixed to_gif function

--- src/to_gif.py
+++ src/to_gif.py
@@ -1,15 +1,26 @@
 from PIL import Image
 import glob
+import re
+import collections
 
 # Create the frames
 frames = []
 imgs = glob.glob(r"../figures/von_karman_vortex_shedding/all_png/*.png")
-for i in imgs:
-    new_frame = Image.open(i)
+regex = re.compile(r'\d+')
+numbers = [int(x) for img in imgs for x in regex.findall(img)]
+
+img_dict = {
+    img: number for img, number in zip(imgs, numbers)
+}
+
+ordered_img_dict = collections.OrderedDict(sorted(img_dict.items(), key=lambda item: item[1]))
+
+for img, _ in ordered_img_dict.items():
+    new_frame = Image.open(img)
     frames.append(new_frame)
 
 # Save into a GIF file that loops forever
 frames[0].save('../figures/von_karman_vortex_shedding/png_to_gif.gif', format='GIF',
                append_images=frames[1:],
                save_all=True,
-               duration=500, loop=0)
+               duration=10, loop=0)
