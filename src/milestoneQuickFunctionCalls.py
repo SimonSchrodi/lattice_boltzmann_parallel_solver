Fixed pouseuille flow bug and added computation of frequency for von karman street

src/milestoneQuickFunctionCalls.py
@@ -1,7 +1,6 @@
 import numpy as np
 from scipy.optimize import curve_fit, least_squares
 import matplotlib.pyplot as plt
-from scipy.fft import fft, fftfreq, fftshift
 
 from mpi4py import MPI
 
@@ -274,31 +273,32 @@ def milestone_6():
         f_post_streaming = rigid_object(plate_boundary.astype(np.bool))(f_pre_streaming, f_post_streaming)
         return f_post_streaming
 
-    density, velocity = density_1_velocity_x_u0_velocity_y_0_initial((lx, ly), u0)
-    f = equilibrium_distr_func(density, velocity)
-    vel_at_p = [np.linalg.norm(velocity[p_coords[0], p_coords[1], ...])]
-    for i in range(time_steps):
-        print(i, np.sum(compute_density(f)))
-        f, density, velocity = lattice_boltzman_step(f, density, velocity, omega, boundary)
-        vel_at_p.append(np.linalg.norm(velocity[p_coords[0], p_coords[1], ...]))
-        if i % 100 == 0:
-            absolute_velocity = np.linalg.norm(velocity, axis=-1)
-            normalized_abs_velocity = absolute_velocity / np.amax(absolute_velocity)
-            from PIL import Image
-            from matplotlib import cm
-            img = Image.fromarray(np.uint8(cm.viridis(normalized_abs_velocity.T) * 255))
-            img.save(r'../figures/von_karman_vortex_shedding/all_png/' + str(i) + '.png')
-            # plt.imshow(absolute_velocity.T)
-            # plt.plot(lx // 4 * np.ones(ly)[ly // 2 - d // 2:ly // 2 + d // 2] + 0.5,
-            #         np.arange(0, ly)[ly // 2 - d // 2:ly // 2 + d // 2], c='red')
-            # plt.colorbar()
-            # plt.show()
+    if False:
+        density, velocity = density_1_velocity_x_u0_velocity_y_0_initial((lx, ly), u0)
+        f = equilibrium_distr_func(density, velocity)
+        vel_at_p = [np.linalg.norm(velocity[p_coords[0], p_coords[1], ...])]
+        for i in range(time_steps):
+            print(i, np.sum(compute_density(f)))
+            f, density, velocity = lattice_boltzman_step(f, density, velocity, omega, boundary)
+            vel_at_p.append(np.linalg.norm(velocity[p_coords[0], p_coords[1], ...]))
+            if i % 100 == 0:
+                absolute_velocity = np.linalg.norm(velocity, axis=-1)
+                normalized_abs_velocity = absolute_velocity / np.amax(absolute_velocity)
+                from PIL import Image
+                from matplotlib import cm
+                img = Image.fromarray(np.uint8(cm.viridis(normalized_abs_velocity.T) * 255))
+                img.save(r'../figures/von_karman_vortex_shedding/all_png/' + str(i) + '.png')
+                # plt.imshow(absolute_velocity.T)
+                # plt.plot(lx // 4 * np.ones(ly)[ly // 2 - d // 2:ly // 2 + d // 2] + 0.5,
+                #         np.arange(0, ly)[ly // 2 - d // 2:ly // 2 + d // 2], c='red')
+                # plt.colorbar()
+                # plt.show()
 
-    np.save(r'../figures/von_karman_vortex_shedding/vel_at_p.py', vel_at_p)
+    #np.save(r'../figures/von_karman_vortex_shedding/vel_at_p.py', vel_at_p)
     vel_at_p = np.load(r'../figures/von_karman_vortex_shedding/vel_at_p.py.npy')
-    np.save(r'../figures/von_karman_vortex_shedding/velocity.py', velocity)
+    #np.save(r'../figures/von_karman_vortex_shedding/velocity.py', velocity)
     velocity = np.load(r'../figures/von_karman_vortex_shedding/velocity.py.npy')
-    np.save(r'../figures/von_karman_vortex_shedding/density.py', density)
+    #np.save(r'../figures/von_karman_vortex_shedding/density.py', density)
     density = np.load(r'../figures/von_karman_vortex_shedding/density.py.npy')
     absolute_velocity = np.linalg.norm(velocity, axis=-1)
     normalized_abs_velocity = absolute_velocity / np.amax(absolute_velocity)
@@ -309,32 +309,19 @@ def milestone_6():
     plt.plot(np.arange(0, time_steps + 1), vel_at_p, linewidth=0.3)
     plt.show()
 
-    frequencies = []
-    vel_at_p -= np.mean(vel_at_p)
-    yf = fft(vel_at_p)
-    xf = fftfreq(len(vel_at_p), 1)
-    # xf = fftshift(xf)
-    frequencies.append(np.abs(yf))
-    plt.plot(xf, np.abs(yf))
+    vel_at_p = vel_at_p[70000:]
+
+    plt.plot(vel_at_p)
     plt.show()
 
-    for i in range(2, 10):
-        vels = vel_at_p[len(vel_at_p) - int(len(vel_at_p) / i):]
-        plt.plot(np.arange(0, int(len(vel_at_p) / i)), vels, linewidth=0.3)
-        plt.show()
-        yf = fft(vels)
-        yf = np.abs(yf)
-        xf = fftfreq(len(vels), 1)
-        max_freq = xf[np.argmax(yf)]
-        # xf = fftshift(xf)
-        frequencies.append(np.abs(yf))
-        plt.plot(xf, np.abs(yf))
-        plt.show()
-        print(xf[np.argmax(frequencies[-1])], xf[np.argmax(frequencies[-2])])
-        if np.argmax(frequencies[-1]) != 0 and np.argmax(frequencies[-1]) == np.argmax(frequencies[-2]):
-            break
+    yf = np.fft.fft(vel_at_p)
+    freq = np.fft.fftfreq(len(vel_at_p), 1)
 
-    vortex_frequency = np.argmax(frequencies)
+    plt.plot(freq, np.abs(yf.imag))
+    plt.show()
+
+    vortex_frequency = np.abs(freq[np.argmax(yf.imag)])
+    print(vortex_frequency)
     strouhal = strouhal_number(vortex_frequency, d, u0)
     print(strouhal)
     reynolds = reynolds_number(d, u0, kinematic_viscosity)
@@ -362,6 +349,7 @@ def milestone_7():
 
     n_local_x = lx // size
     n_local_y = ly // size
+    # TODO rechten & oberen finden
     if rank == size - 1:
         n_local_x = lx - n_local_x * (size - 1)
         n_local_y = ly - n_local_y * (size - 1)
@@ -369,11 +357,11 @@ def milestone_7():
     left_dst, right_dst = cartesian3d.Shift(direction=0, disp=1)
     bottom_dst, top_dst = cartesian3d.Shift(direction=1, disp=1)
 
-    def boundary(coord3d, n_local_x, n_local_y):
-        max_coord_x = np.amax(coord3d[:, 0])
+    def boundary(coord2d, n_local_x, n_local_y):
+        max_coord_x = np.amax(coord2d[:, 0])
 
         def bc(f_pre_streaming, f_post_streaming, density=None, velocity=None, f_previous=None):
-            if coord3d[0] == 0:
+            if coord2d[0] == 0:
                 f_post_streaming[1:-1, 1:-1, :] = inlet((n_local_x, n_local_y), density_in, u0)(
                     f_post_streaming[1:-1, 1:-1, :])
 
@@ -411,32 +399,19 @@ def milestone_7():
     plt.plot(np.arange(0, time_steps + 1), vel_at_p, linewidth=0.3)
     plt.show()
 
-    frequencies = []
-    vel_at_p -= np.mean(vel_at_p)
-    yf = fft(vel_at_p)
-    xf = fftfreq(len(vel_at_p), 1)
-    # xf = fftshift(xf)
-    frequencies.append(np.abs(yf))
-    plt.plot(xf, np.abs(yf))
+    vel_at_p = vel_at_p[70000:]
+
+    plt.plot(vel_at_p)
     plt.show()
 
-    for i in range(2, 10):
-        vels = vel_at_p[len(vel_at_p) - int(len(vel_at_p) / i):]
-        plt.plot(np.arange(0, int(len(vel_at_p) / i)), vels, linewidth=0.3)
-        plt.show()
-        yf = fft(vels)
-        yf = np.abs(yf)
-        xf = fftfreq(len(vels), 1)
-        max_freq = xf[np.argmax(yf)]
-        # xf = fftshift(xf)
-        frequencies.append(np.abs(yf))
-        plt.plot(xf, np.abs(yf))
-        plt.show()
-        print(xf[np.argmax(frequencies[-1])], xf[np.argmax(frequencies[-2])])
-        if np.argmax(frequencies[-1]) != 0 and np.argmax(frequencies[-1]) == np.argmax(frequencies[-2]):
-            break
+    yf = np.fft.fft(vel_at_p)
+    freq = np.fft.fftfreq(len(vel_at_p), 1)
 
-    vortex_frequency = np.argmax(frequencies)
+    plt.plot(freq, np.abs(yf.imag))
+    plt.show()
+
+    vortex_frequency = np.abs(freq[np.argmax(yf.imag)])
+    print(vortex_frequency)
     strouhal = strouhal_number(vortex_frequency, d, u0)
     print(strouhal)
     reynolds = reynolds_number(d, u0, kinematic_viscosity)

src/plot_results_for_paper.py
@@ -277,7 +277,7 @@ def plot_couette_flow_vel_vectors(lattice_grid_shape: Tuple[int, int] = (20, 30)
              linewidth=1.5, c='green',
              linestyle='-')
     plt.ylabel('y position [lu]')
-    plt.xlabel(r'velocity in y-direction $u$ [$\frac{m}{s}$]')
+    plt.xlabel(r'velocity in y-direction $u$ [$\frac{lu}{s}$]')
     plt.legend()
 
     plt.rc('text', usetex=True)
@@ -316,16 +316,16 @@ def plot_couette_flow_vel_vectors(lattice_grid_shape: Tuple[int, int] = (20, 30)
     plt.savefig(r'../figures/couette_flow/relative_error.svg', bbox_inches='tight')
 
 
-def plot_poiseuille_flow_vel_vectors(lattice_grid_shape: Tuple[int, int] = (200, 30),
+def plot_poiseuille_flow_vel_vectors(lattice_grid_shape: Tuple[int, int] = (200, 60),
                                      omega: float = 1.5,
-                                     delta_p: float = 0.001111,
-                                     time_steps: int = 5000):
+                                     delta_p: float = 0.001,
+                                     time_steps: int = 20000):
     lx, ly = lattice_grid_shape
 
     rho_0 = 1
     delta_rho = delta_p * 3
-    rho_inlet = rho_0 + delta_rho
-    rho_outlet = rho_0
+    rho_inlet = rho_0 + (delta_rho/2)
+    rho_outlet = rho_0 - (delta_rho/2)
     p_in = rho_inlet / 3
     p_out = rho_outlet / 3
 
@@ -353,46 +353,66 @@ def plot_poiseuille_flow_vel_vectors(lattice_grid_shape: Tuple[int, int] = (200,
         f, density, velocity = lattice_boltzman_step(f, density, velocity, omega, boundary)
 
     vx = velocity[..., 0]
-    x_coord = lx // 2
+    x_coords = [1, lx // 2]
     centerline = ly // 2
 
-    for vec, y_coord in zip(vx[x_coord, :], np.arange(0, ly)):
-        origin = [0, y_coord]
-        plt.quiver(*origin, *[vec, 0.0], color='blue', scale_units='xy', scale=1, headwidth=3, width=0.0025)
-    plt.plot(vx[x_coord, :], np.arange(0, ly), label='Sim. sol.', linewidth=1, c='blue', linestyle=':')
+    areas = []
+    colors = ['cyan', 'blue']
+    linestyle = [':', '-.']
+    for c, ls, x_coord in zip(colors, linestyle, x_coords):
+        # for vec, y_coord in zip(vx[x_coord, :], np.arange(0, ly)):
+        #     origin = [0, y_coord]
+        #     plt.quiver(*origin, *[vec, 0.0], color='blue', scale_units='xy', scale=1, headwidth=3, width=0.0025)
+        plt.plot(vx[x_coord, :], np.arange(0, ly), label='Sim. sol. channel '+str(x_coord), linewidth=1, c=c, linestyle=ls)
+        areas.append(
+            np.trapz(vx[x_coord, :], np.arange(0, ly))
+        )
+        viscosity = (1 / 3) * (1 / omega - 0.5)
+        dynamic_viscosity = viscosity * np.mean(density[x_coord, :])
+        h = ly
+        y = np.arange(0, ly + 1)
+        dp_dx = np.divide(p_out - p_in, lx)
+        uy = -np.reciprocal(2 * dynamic_viscosity) * dp_dx * y * (h - y)
+        plt.plot(uy, y - 0.5, label='Analyt. sol.', c='red',
+                 linestyle='--')
 
-    viscosity = (1 / 3) * (1 / omega - 0.5)
-    dynamic_viscosity = viscosity * np.mean(density[x_coord, :])
-    h = ly
-    y = np.arange(0, ly + 1)
-    dp_dx = np.divide(p_out - p_in, lx)
-    uy = -np.reciprocal(2 * dynamic_viscosity) * dp_dx * y * (h - y)
-    plt.plot(uy, y - 0.5, label='Analyt. sol.', c='red',
-             linestyle='--')
+        plt.plot(np.linspace(0, np.amax(vx) * 1.05, 100), np.zeros_like(np.linspace(0, np.amax(vx) * 1.05, 100)) - 0.5,
+                 label='Rigid wall',
+                 linewidth=1.5, c='green',
+                 linestyle='-')
+        plt.plot(np.linspace(0, np.amax(vx) * 1.05, 100),
+                 np.ones_like(np.linspace(0, np.amax(vx) * 1.05, 100)) * (ly - 1) + 0.5,
+                 label='Rigid wall',
+                 linewidth=1.5, c='green',
+                 linestyle='-')
 
-    plt.plot(np.linspace(0, np.amax(vx) * 1.05, 100), np.zeros_like(np.linspace(0, np.amax(vx) * 1.05, 100)) - 0.5,
-             label='Rigid wall',
-             linewidth=1.5, c='green',
-             linestyle='-')
-    plt.plot(np.linspace(0, np.amax(vx) * 1.05, 100),
-             np.ones_like(np.linspace(0, np.amax(vx) * 1.05, 100)) * (ly - 1) + 0.5,
-             label='Rigid wall',
-             linewidth=1.5, c='green',
-             linestyle='-')
+        plt.ylabel('y position [lu]')
+        plt.xlabel(r'velocity in y-direction $u$ [$\frac{lu}{s}$]')
+        handles, labels = plt.gca().get_legend_handles_labels()
+        by_label = OrderedDict(zip(labels, handles))
+        plt.legend(by_label.values(), by_label.keys())
+        plt.legend(by_label.values(), by_label.keys(), loc='lower right')
 
-    plt.ylabel('y position [lu]')
-    plt.xlabel(r'velocity in y-direction $u$ [$\frac{m}{s}$]')
-    handles, labels = plt.gca().get_legend_handles_labels()
-    by_label = OrderedDict(zip(labels, handles))
-    plt.legend(by_label.values(), by_label.keys())
-    plt.legend(by_label.values(), by_label.keys(), loc='lower right')
+        plt.rc('text', usetex=True)
+        plt.rc('font', family='serif')
 
-    plt.rc('text', usetex=True)
-    plt.rc('font', family='serif')
-
-    plt.savefig(r'../figures/poiseuille_flow/vel_vectors.svg', bbox_inches='tight')
+        plt.savefig(r'../figures/poiseuille_flow/vel_vectors.svg', bbox_inches='tight')
 
     plt.close()
+
+    areas.append(areas[0]/areas[1])
+    areas = np.array(areas)
+    with open('../figures/poiseuille_flow/areas.csv', 'w', newline='') as csvfile:
+        fieldnames = ['inlet', 'middle', 'relative_difference']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerow(
+            {
+                'inlet': areas[0],
+                'middle': areas[1],
+                'relative_difference': areas[2]
+            }
+        )
 
     plt.plot(np.arange(0, lx - 2), density[1:-1, centerline] / 3, label='Pressure along centerline')
     plt.plot(np.arange(0, lx - 2), np.ones_like(np.arange(0, lx - 2)) * p_out, label='Outgoing pressure')